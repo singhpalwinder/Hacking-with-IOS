4ce793fd2227409c73949677117c4b32a931ad93
Palwinder Singh <38331930+singhpalwinder@users.noreply.github.com>
2024-07-09 15:09:22 -0500

    Add files via upload

---
+++ Proj-1-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Palwinder Singh on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
